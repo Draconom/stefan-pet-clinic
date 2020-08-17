d29decdcd2241c914af320b0675d015d97f194b4
[Draconom] <[stefan.srbljanin@gmail.com]>
2020-08-17 11:18:09 +0200

    Added new class - BaseEntity that handles with Long id Has a getter and a setter close #10

--- pet-clinic-data/src/main/java/stefan/springframework/stefanpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/stefan/springframework/stefanpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package stefan.springframework.stefanpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
 
     private String firstName, lastName;
 
--- pet-clinic-data/src/main/java/stefan/springframework/stefanpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/stefan/springframework/stefanpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package stefan.springframework.stefanpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
